d2f05eed8cc50bacec1de39b10c5ac8e6285f88b
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-12-05 13:58:46 +0100

    Updated

--- 0x03-ES6_data_manipulation/100-main.js
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -1,4 +1,4 @@
-import { queryAPI, weakMap } from "./100-weak";
+import { queryAPI, weakMap } from './100-weak';
 
 const endpoint = { protocol: 'http', name: 'getUsers' };
 weakMap.get(endpoint);
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-	if (!startString || startString.length === 0) {
-		return '';
-	}
+  if (!startString || startString.length === 0) {
+    return '';
+  }
 
-	const cleanedValues = [...set]
-		.filter(parametro => parametro !== undefined && parametro.startsWith(startString))
-		.map(parametro => parametro !== undefined ? parametro.slice(startString.length) : '');
+  const cleanedValues = [...set]
+    .filter((parametro) => parametro !== undefined && parametro.startsWith(startString))
+    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''));
 
-	return cleanedValues.join('-');
+  return cleanedValues.join('-');
 }
