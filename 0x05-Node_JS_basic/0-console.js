e09c79b03aa447d46fc7d0ca8e1f77be6cad5af5
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-01-31 13:40:00 +0100

    Updated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 // File: 0-console.js
 
 function displayMessage(message) {
-	console.log(message);
-  }
+  console.log(message);
+}
   
-  module.exports = displayMessage;
+module.exports = displayMessage;
