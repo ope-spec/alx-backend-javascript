a6c6b79f7609844f5d12ababbebf624d32de6f5b
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-02-01 14:45:03 +0100

    Updated

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,27 +1,62 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jest/no-test-return-statement */
+/* eslint-disable jest/valid-expect */
+
 const request = require('request');
+const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
-describe('Login endpoint', function() {
-  it('should return correct status code and result', function(done) {
-    request.post('http://localhost:7865/login', { json: { userName: 'Betty' } }, (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome Betty');
-      done();
+describe('login endpoint', () => {
+  const option = {
+    url: 'http://localhost:7865/',
+    method: 'GET',
+  };
+  it('check correct status code', () => {
+    expect.hasAssertions();
+    return new Promise((done) => {
+      request(option, (err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+  it('check correct content', () => {
+    expect.hasAssertions();
+    return new Promise((done) => {
+      request(option, (err, res, body) => {
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
   });
 });
 
-describe('Available payments endpoint', function() {
-  it('should return correct status code and result', function(done) {
-    request('http://localhost:7865/available_payments', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(JSON.parse(body)).to.deep.equal({
-        payment_methods: {
-          credit_cards: true,
-          paypal: false
-        }
-      });
-      done();
+describe('available payments endpoint', () => {
+  it('check correct status for correct url', () => {
+    expect.hasAssertions();
+    request.get('http://localhost:7865/available_payments', (err, res) => {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      } else {
+        expect(res.statusCode).to.equal(200);
+      }
+    });
+  });
+  it('check correct body content for correct url', () => {
+    expect.hasAssertions();
+    const option = { json: true };
+    const payLoad = {
+      payment_methods: {
+        credit_cards: true,
+        paypal: false,
+      },
+    };
+    request.get('http://localhost:7865/available_payments', option, (err, res, body) => {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      } else {
+        expect(body).to.deep.equal(payLoad);
+      }
     });
   });
 });
