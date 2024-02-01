86858f25b05aee677267878854dcad5eb165b685
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-02-01 15:12:41 +0100

    Updated

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,8 @@
 function calculateNumber(a, b) {
-    const c = Math.round(a);
-    const d = Math.round(b);
-    const e = c + d;
-    return e;
+  const c = Math.round(a);
+  const d = Math.round(b);
+  const e = c + d;
+  return e;
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { it, describe } = require('mocha');
+const { describe, it } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber()', () => {
@@ -7,12 +7,39 @@ describe('calculateNumber()', () => {
     const res = calculateNumber(1, 3);
     assert.strictEqual(res, 4);
   });
+
   it('should return 5 when adding 1 and 3.7', () => {
     const res = calculateNumber(1, 3.7);
     assert.strictEqual(res, 5);
   });
+
   it('should return 6 when adding 1.5 and 3.7', () => {
     const res = calculateNumber(1.5, 3.7);
     assert.strictEqual(res, 6);
+  });
+
+  it('should return 0 when adding 0 and 0', () => {
+    const res = calculateNumber(0, 0);
+    assert.strictEqual(res, 0);
+  });
+
+  it('should return -1 when adding -1 and 0', () => {
+    const res = calculateNumber(-1, 0);
+    assert.strictEqual(res, -1);
+  });
+
+  it('should return 1 when adding -0.5 and 1.5', () => {
+    const res = calculateNumber(-0.5, 1.5);
+    assert.strictEqual(res, 2);
+  });
+
+  it('should return -1 when adding -1.5 and -0.5', () => {
+    const res = calculateNumber(-1.5, -0.5);
+    assert.strictEqual(res, -1);
+  });
+
+  it('should return 100 when adding 50 and 50', () => {
+    const res = calculateNumber(50, 50);
+    assert.strictEqual(res, 100);
   });
 });
