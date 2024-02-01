c50f2c25f869928f580832713f7f3d475e18036a
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-02-01 14:29:48 +0100

    Updated

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -1,37 +1,35 @@
-/* eslint-disable jest/prefer-expect-assertions */
-
 const { expect } = require('chai');
 
 describe('testing numbers', () => {
   it('1 is equal to 1', () => {
-    expect(1).to.equal(1);
+    expect(1 === 1).to.be.true;
   });
 
   it('2 is equal to 2', () => {
-    expect(2).to.equal(2);
+    expect(2 === 2).to.be.true;
   });
 
-  it('1 is equal to 3', () => {
-    expect(1).to.equal(3);
+  it.skip('1 is equal to 3', () => {
+    expect(1 === 3).to.be.true;
   });
 
   it('3 is equal to 3', () => {
-    expect(3).to.equal(3);
+    expect(3 === 3).to.be.true;
   });
 
   it('4 is equal to 4', () => {
-    expect(4).to.equal(4);
+    expect(4 === 4).to.be.true;
   });
 
   it('5 is equal to 5', () => {
-    expect(5).to.equal(5);
+    expect(5 === 5).to.be.true;
   });
 
   it('6 is equal to 6', () => {
-    expect(6).to.equal(6);
+    expect(6 === 6).to.be.true;
   });
 
   it('7 is equal to 7', () => {
-    expect(7).to.equal(7);
+    expect(7 === 7).to.be.true;
   });
 });
