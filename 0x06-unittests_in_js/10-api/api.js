f43051884d9591faae8908d959358e0c27143074
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-02-02 15:00:32 +0100

    tired

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -9,31 +9,36 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
-  res.send(`Payment methods for cart ${req.params.id}`);
+app.get('/cart/:id', (req, res) => {
+  const cartId = req.params.id;
+  if (!isNaN(cartId)) {
+    res.send(`Payment methods for cart ${cartId}`);
+  } else {
+    res.status(400).send('Invalid cart ID');
+  }
 });
 
 app.get('/available_payments', (req, res) => {
-  const payMethods = {
+  const paymentMethods = {
     payment_methods: {
       credit_cards: true,
       paypal: false,
     },
   };
-  res.json(payMethods);
+  res.json(paymentMethods);
 });
 
 app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  if (userName) {
-    res.send(`Welcome ${userName}`);
+  const { username } = req.body;
+  if (username) {
+    res.send(`Welcome ${username}`);
   } else {
-    res.status(404).send();
+    res.status(400).send('Username is required');
   }
 });
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
 
-module.exports = server;
+module.exports = app;
