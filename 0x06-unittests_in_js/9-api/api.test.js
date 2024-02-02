39810d075eefc86adb831f7ca0c62a897d8e8efc
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-02-02 14:42:52 +0100

    9-api

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,19 +1,19 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Cart page', function() {
-  it('should return correct status code and result when :id is a number', function(done) {
+describe('cart page', () => {
+  it('should return correct status code and result when :id is a number', () => new Promise((done) => {
     request('http://localhost:7865/cart/12', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Payment methods for cart 12');
       done();
     });
-  });
+  }));
 
-  it('should return 404 status code when :id is NOT a number', function(done) {
+  it('should return 404 status code when :id is NOT a number', () => new Promise((done) => {
     request('http://localhost:7865/cart/hello', (error, response, body) => {
       expect(response.statusCode).to.equal(404);
       done();
     });
-  });
+  }));
 });
